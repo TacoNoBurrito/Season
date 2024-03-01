43a2b11e9ee888f05791541c502ae3001773d2c7
TacoNoBurrito <74216683+TacoNoBurrito@users.noreply.github.com>
2024-03-01 03:56:13 +0000

    added tab cloaking, need to finish themes though.

--- static/scripts/home.js
+++ static/scripts/home.js
@@ -5,5 +5,18 @@ if (localStorage.getItem("theme") == null) {
         icon: "assets/images/logo.png"
     }));
     localStorage.setItem("favoriteGames", []);
+    localStorage.setItem("cloaking", 0);
+}
+
+if (localStorage.getItem("cloaking") == 1) {
+    const win = window.open();
+    const iframe = win.document.createElement("iframe");
+    iframe.style.width = "100%";
+    iframe.style.height = "100%";
+    iframe.style.border = "none";
+    iframe.src = window.location;
+    win.document.body.style.margin = "0 0";
+    win.document.body.appendChild(iframe);
+    window.close();
 }
 
--- static/scripts/settings.js
+++ static/scripts/settings.js
@@ -3,12 +3,14 @@ document.getElementById("title").value = cloakData.title;
 document.getElementById("icon").value = cloakData.icon;
 const currentTheme = localStorage.getItem("theme");
 const themeElement = document.getElementById("theme");
+const cloakElement = document.getElementById("cloaking");
 for (var i = 0; i < themeElement.options.length; i++) {
     if (themeElement.options[i].text === currentTheme) {
         themeElement.selectedIndex = i;
         break;
     }
 }
+cloakElement.selectedIndex = localStorage.getItem("cloaking");
 
 window.addEventListener("beforeunload", () => {
     localStorage.setItem("cloakData", JSON.stringify({
@@ -16,4 +18,5 @@ window.addEventListener("beforeunload", () => {
         icon: document.getElementById("icon").value
     }));
     localStorage.setItem("theme", themeElement.options[themeElement.selectedIndex].text);
+    localStorage.setItem("cloaking", cloakElement.selectedIndex);
 });
